2d7feea82454c51cecb1b0c033674d97ee5e3a8f
Nick Sinklier <nicksinklier@protonmail.com>
2025-03-23 14:34:52 -0700

    Started SearchView

---
+++ Movies/Movies/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Movies
+//
+//  Created by Nick Sinklier on 3/22/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
